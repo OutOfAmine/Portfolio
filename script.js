534774642087f9ff5c5fa726e33c17a947802c71
amine jerrary <aminejerrary.com>
2023-03-26 23:29:59 +0000

    landing page +experiences

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+
+
+// if (window.innerWidth>750) {
+//     document.querySelector('.info-container').style.display='flex';
+//     document.querySelector('.nav').style.display='flex';
+// }
+// function ShowNavMobile() {
+//             if (a) { 
+//                 document.querySelector('.info-container').style.display='none';
+//                 document.querySelector('.nav').style.display='flex';
+//                     a=false
+//         }
+//         else{
+//             document.querySelector('.info-container').style.display='flex';
+//             document.querySelector('.nav').style.display='none';
+//                 a=true;
+//     }
+
+// }
 var a=true;
 function ShowNavMobile() {
-    if (a) {
-    document.querySelector('.info-container').style.display='none';
-    document.querySelector('.nav').style.display='flex';
-        a=false
+    var nav = document.querySelector('.nav');
+    var infoContainer = document.querySelector('.info-container');
+    if (window.innerWidth > 750) {
+      nav.style.display = 'flex';
+      infoContainer.style.display = 'flex';
+    } else {
+        if (a) { 
+                            document.querySelector('.info-container').style.display='none';
+                            document.querySelector('.nav').style.display='flex';
+                                a=false
+                    }
+                    else{
+                        document.querySelector('.info-container').style.display='flex';
+                        document.querySelector('.nav').style.display='none';
+                            a=true;
+                }
+    
     }
-    else{
-    document.querySelector('.info-container').style.display='flex';
-    document.querySelector('.nav').style.display='none';
-        a=true;
-    }
-}
+  }
+  
+  // Add an event listener for the resize event
+  window.addEventListener('resize', ShowNavMobile);
 gsap.registerPlugin(ScrollTrigger)
 const experiences=document.querySelectorAll('.row')
 let animateRight = true;
