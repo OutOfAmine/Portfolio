88369e17ecebc9be8c6cc466fe33a84dbe5c348c
amine jerrary <aminejerrary.com>
2023-03-26 07:24:21 +0000

    landing page +experiences

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function ShowNavMobile() {
     }
 }
 gsap.registerPlugin(ScrollTrigger)
-const experiences_left=document.querySelectorAll('.row-1')
-for (let i = 0; i < experiences_left.length; i++) {
-experiences_left[i]
-gsap.fromTo(experiences_left[i],{x:-300,opacity:0},{x:0,duration:0.8,opacity:1,scrollTrigger:{trigger:experiences_left[i],start:'top center'}})
+const experiences=document.querySelectorAll('.row')
+let animateRight = true;
+for (let i = 0; i < experiences.length; i++) {
+if (animateRight) {
+    gsap.fromTo(experiences[i],{x:-300,opacity:0},{x:0,duration:0.8,opacity:1,scrollTrigger:{trigger:experiences[i],start:'top center'}})
 }
-const experiences_right=document.querySelectorAll('.row-2')
-for (let i = 0; i < experiences_right.length; i++) {
-experiences_right[i]
-gsap.fromTo(experiences_right[i],{x:+300,opacity:0},{x:0,duration:0.8,opacity:1,scrollTrigger:{trigger:experiences_right[i],start:'top center'}})
 }
+
+
+
